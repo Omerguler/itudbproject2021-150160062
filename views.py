30dbd0e126fe7edb113bf74f7f159ea389377882
omer@letspalm.com <kerpetenci@gmail.com>
2021-01-20 05:12:25 +0300

    Add db values

--- views.py
+++ views.py
@@ -8,6 +8,17 @@ from flask_login import LoginManager, login_user, logout_user, login_required, c
 
 def sign_in():
     form = SigninForm(request.form)
+    database = Database()
+    database.create_user()
+    database.create_admin()
+    database.create_admin_added()
+    database.create_favorites()
+    database.create_friends()
+    database.create_movie()
+    database.create_ratings()
+    database.add_user(User(0, "admin_account", "123456789",
+                           "1998-08-06", "admin@gmail.com"))
+    database.add_admin(1)
     if request.method == 'POST' and form.validate_on_submit():
         username = form.username.data
         password = form.password.data
